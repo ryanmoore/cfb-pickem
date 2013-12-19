6402e2cb5ac9f81d5482811cbfd891b4a28f0676
Ryan Moore <ryanmoore88@gmail.com>
2013-12-19 13:03:41 -0500

    Tightened up CSS style for selection to fit more rows on a page.

--- pickem/static/pickem/js/select_all_form.js
+++ pickem/static/pickem/js/select_all_form.js
@@ -1,12 +1,12 @@
 function onSubmit() {
-var order = $( '#matchup_ranking' ).sortable( 'toArray' );
-$( "#matchup_ordering" ).val( JSON.stringify(order) );
+    var order = $( '#matchup_ranking' ).sortable( 'toArray' );
+    $( "#matchup_ordering" ).val( JSON.stringify(order) );
 }
 
 function configure_sortable() {
-$( '#matchup_ranking').sortable({ axis: 'y' });
-$( '#matchup_ranking' ).disableSelection();
-$( 'form' ).submit( onSubmit );
+    $( '#matchup_ranking').sortable({ axis: 'y' });
+    $( '#matchup_ranking' ).disableSelection();
+    $( 'form' ).submit( onSubmit );
 }
 
 $(document).ready( configure_sortable );
