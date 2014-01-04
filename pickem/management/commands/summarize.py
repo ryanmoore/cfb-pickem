543f19d97ef2214cce07b993e1338489a6c70134
Ryan Moore <ryanmoore88@gmail.com>
2014-01-03 20:39:52 -0500

    Dumping matchup data and team mappings to summary.

--- pickem/management/commands/summarize.py
+++ pickem/management/commands/summarize.py
@@ -8,6 +8,7 @@ import json
 import logging
 import datetime
 import re
+import itertools
 from pickem.views import ScoreTable
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,10 @@ class Command(BaseCommand):
     args = '<output>'
     help = 'Writes current status and future pick info to file as json'
 
+    def __init__(self):
+        super().__init__()
+        self.score_table = ScoreTable(User.objects.all())
+
     def handle(self, *args, **options):
         ''' Main function manage calls. Reads json and adds to database
         '''
@@ -26,7 +31,26 @@ class Command(BaseCommand):
         filename = args[0]
 
         with transaction.atomic():
-            scores = ScoreTable(User.objects.all()).scores_as_table()
+            scores = self.score_table.scores_as_table()
             scores = dict([ ( str(user), score ) for user, score in scores ] )
+            matchups = self.remaining_picks()
+            matchup_ids = [ ( x.id, y.id ) for x,y in matchups ]
+            participants = itertools.chain(*matchups)
+            teams = dict( [ (part.id, part.team.name) for part in participants] )
+
+            data = {
+                    'scores': scores,
+                    'matchups' : matchup_ids,
+                    'teams' : teams,
+                    }
             with open(filename, 'w') as outfile:
-                json.dump(scores, outfile, indent=4)
+                json.dump(data, outfile, indent=4)
+
+    def remaining_picks(self):
+        games = self.score_table.unplayed_games()
+        return [ game_participants(game) for game in games ]
+
+def game_participants(game):
+    return game.participant_set.all()
+
+
