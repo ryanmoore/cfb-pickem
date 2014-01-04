0890be6edbeb21f3f301bc90543eeb8a3e0538c5
Ryan Moore <ryanmoore88@gmail.com>
2014-01-03 22:30:06 -0500

    Generating list of user picks in summarize function.

--- pickem/management/commands/summarize.py
+++ pickem/management/commands/summarize.py
@@ -1,7 +1,7 @@
 ''' Prints users and scores and upcoming games and wagers
 '''
 from django.core.management.base import BaseCommand, CommandError
-#from pickem.models import Event, Game, Team, Participant
+from pickem.models import Wager, Selection
 from django.db import transaction
 from django.contrib.auth.models import User
 import json
@@ -33,22 +33,39 @@ class Command(BaseCommand):
         with transaction.atomic():
             scores = self.score_table.scores_as_table()
             scores = dict([ ( str(user), score ) for user, score in scores ] )
-            matchups = self.remaining_picks()
+            matchups = self.remaining_matchups()
             matchup_ids = [ ( x.id, y.id ) for x,y in matchups ]
-            participants = itertools.chain(*matchups)
+            participants = list(itertools.chain(*matchups))
             teams = dict( [ (part.id, part.team.name) for part in participants] )
-
+            picks = list(self.get_picks(participants))
             data = {
                     'scores': scores,
                     'matchups' : matchup_ids,
                     'teams' : teams,
+                    'picks' : picks,
                     }
             with open(filename, 'w') as outfile:
                 json.dump(data, outfile, indent=4)
 
-    def remaining_picks(self):
+    def remaining_matchups(self):
         games = self.score_table.unplayed_games()
         return [ game_participants(game) for game in games ]
+
+    def get_picks(self, participants):
+        wagers = Wager.objects.filter(game__in=self.score_table.unplayed_games())
+        for w in wagers:
+            try:
+                amount = w.amount
+                part = Selection.objects.get(user=w.user, participant__game=w.game).participant
+            except Selection.DoesNotExist:
+                amount = 0
+                part = list(w.game.participant_set.all())[0]
+            yield (
+                    { 'user': w.user.id,
+                      'wager' : amount,
+                      'team' : part.id,
+                      }
+                )
 
 def game_participants(game):
     return game.participant_set.all()
