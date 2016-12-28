7af464a0aa1af9e27942aa79bba0bba537051d57
Ryan Moore <ryanmoore88@gmail.com>
2016-12-28 00:50:11 -0500

    Updated scores page to sort scores by Achieved then Total Possible then Username

--- pickem-react/src/Components/ScoresTable.js
+++ pickem-react/src/Components/ScoresTable.js
@@ -22,7 +22,20 @@ export default class ScoresTable extends Component {
         const {
             scores
         } = this.props;
-        const scoreBars = scores.map((score) => {
+        const sortByAchievedScoreDesc = (a, b) => {
+            if(a.score < b.score) return 1;
+            if(a.score > b.score) return -1;
+            const aTotal = a.score + a.remaining;
+            const bTotal = b.score + b.remaining;
+            if(aTotal < bTotal) return 1;
+            if(aTotal > bTotal) return -1;
+            if(b.username < a.username) return 1;
+            if(b.username > a.username) return -1;
+            return 0;
+        };
+        var sortedScores = scores.slice(0);
+        sortedScores.sort(sortByAchievedScoreDesc);
+        const scoreBars = sortedScores.map((score) => {
             return <ScoreTableRow 
                     key={score.username}
                     name={score.username}
