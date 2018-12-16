b9a76b635d2a4ec9ed8908c2bda21f5f2bfeb7a3
Ryan Moore <ryanmoore88@gmail.com>
2018-12-16 11:36:11 -0500

    Add upload and install script

---
+++ pickem-react/bundle_and_scp_to_forerunner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "-- Building"
+ln -sf .env.prod .env
+npm run build
+echo "-- Transferring"
+tar czvf ./build.tar.gz ./build && scp ./build.tar.gz forerunner:~/
+ln -sf .env.dev .env
+echo "-- Installing on remote"
+ssh forerunner "cd pickem-react-build; ./install.sh"
