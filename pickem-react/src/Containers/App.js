cb63ab04cfc02cde526754a2ea725068efdc651d
Ryan Moore <ryanmoore88@gmail.com>
2018-12-19 14:15:03 -0500

    Prod and dev app are now the same

--- pickem-react/src/Containers/App.js
+++ pickem-react/src/Containers/App.js
@@ -1,5 +1,98 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./App.prod');
-} else {
-    module.exports = require('./App.dev');
+import React, {
+    Component
+} from 'react';
+import './App.css';
+import {
+    Provider
+} from 'react-redux';
+
+import configureStore from '../store/configureStore';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
+import ViewPicksPage from '../Containers/ViewPicksPage';
+import PickemApp from '../Containers/PickemApp';
+import GameIndex from '../Containers/GameIndex';
+import MakePicksPage from '../Containers/MakePicksPage';
+import ViewScoresPage from '../Containers/ViewScoresPage';
+import LoginPage from '../Containers/LoginPage';
+import LogoutPage from '../Containers/LogoutPage';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from '../auth.js';
+import PageNotFound from '../Components/PageNotFound';
+import AdminViewPicksPage from '../Containers/AdminViewPicksPage';
+import YearSelector from '../Containers/YearSelector';
+import ViewHistoryPage from '../Containers/ViewHistoryPage';
+import { DragDropContextProvider } from 'react-dnd';
+import {
+    default as TouchBackend
+} from 'react-dnd-touch-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+let { store } = configureStore();
+
+
+function withYearSelector(WrappedComponent) {
+    class NewClass extends Component {
+        render() {
+            return (
+                <YearSelector {...this.props}>
+                    <WrappedComponent {...this.props} />
+                </YearSelector>
+            );
+        }
+    }
+    return NewClass;
 }
+
+// TODO: Seems we rarely take the html5 backend. Not working in chrome at
+// least on dev machine
+var dndBackend;
+if ('ontouchstart' in window) {
+    dndBackend = TouchBackend({
+        enableMouseEvents: true
+    });
+} else {
+    dndBackend = HTML5Backend;
+}
+
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <DragDropContextProvider backend={dndBackend}>
+                    <Router>
+                        <div>
+                            <Route path='/' component={PickemApp} />
+                            <Switch>
+                                <Route exact path="/" render={() => (
+                                    <Redirect to="/2018/index"/>
+                                )}/>
+                                <Route path='/login' component={UserIsNotAuthenticated(LoginPage)} />
+                                <Route path='/logout' component={UserIsAuthenticated(LogoutPage)} />
+                                <Route path='/history' component={ViewHistoryPage} />
+                                {/*<Route path='/:year' component={YearSelector}/> */}
+                                {/* <NavRedirect to='index' /> */}
+                                <Route path='/:year/index' component={withYearSelector(GameIndex)} />
+                                <Route path='/:year/makepicks' component={UserIsAuthenticated(withYearSelector(MakePicksPage))} />
+                                <Route path='/:year/picks' component={withYearSelector(ViewPicksPage)} />
+                                <Route path='/:year/scores' component={withYearSelector(ViewScoresPage)} />
+                                <Route path='/:year/admin/addwinner' component={withYearSelector(AdminViewPicksPage)}/>
+                                {/* Make sure this route is last */}
+                                <Route
+                                    path='*'
+                                    render={props => <PageNotFound {...props}
+                                        from='RoutingFallthrough'/>} />
+                            </Switch>
+                        </div>
+                    </Router>
+                </DragDropContextProvider>
+            </Provider>
+        );
+    }
+}
+
+export default App;
