338d23340cd59ba2176220837288ac0f4b24fe3b
Ryan Moore <ryanmoore88@gmail.com>
2018-12-20 08:43:11 -0500

    Move routes back into own file. Remove old, unused routes.

--- pickem-react/src/Containers/App.js
+++ pickem-react/src/Containers/App.js
@@ -5,26 +5,8 @@ import './App.css';
 import {
     Provider
 } from 'react-redux';
-
+import PickemRouter from '../Containers/PickemRouter';
 import configureStore from '../store/configureStore';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-} from 'react-router-dom';
-import ViewPicksPage from '../Containers/ViewPicksPage';
-import PickemApp from '../Containers/PickemApp';
-import GameIndex from '../Containers/GameIndex';
-import MakePicksPage from '../Containers/MakePicksPage';
-import ViewScoresPage from '../Containers/ViewScoresPage';
-import LoginPage from '../Containers/LoginPage';
-import LogoutPage from '../Containers/LogoutPage';
-import { UserIsAuthenticated, UserIsNotAuthenticated } from '../auth.js';
-import PageNotFound from '../Components/PageNotFound';
-import AdminViewPicksPage from '../Containers/AdminViewPicksPage';
-import YearSelector from '../Containers/YearSelector';
-import ViewHistoryPage from '../Containers/ViewHistoryPage';
 import { DragDropContextProvider } from 'react-dnd';
 import {
     default as TouchBackend
@@ -33,19 +15,6 @@ import HTML5Backend from 'react-dnd-html5-backend';
 
 let { store } = configureStore();
 
-
-function withYearSelector(WrappedComponent) {
-    class NewClass extends Component {
-        render() {
-            return (
-                <YearSelector {...this.props}>
-                    <WrappedComponent {...this.props} />
-                </YearSelector>
-            );
-        }
-    }
-    return NewClass;
-}
 
 // TODO: Seems we rarely take the html5 backend. Not working in chrome at
 // least on dev machine
@@ -64,31 +33,7 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <DragDropContextProvider backend={dndBackend}>
-                    <Router basename={process.env.REACT_APP_SITE_BASENAME}>
-                        <div>
-                            <Route path='/' component={PickemApp} />
-                            <Switch>
-                                <Route exact path="/" render={() => (
-                                    <Redirect to="/2018/index"/>
-                                )}/>
-                                <Route path='/login' component={UserIsNotAuthenticated(LoginPage)} />
-                                <Route path='/logout' component={UserIsAuthenticated(LogoutPage)} />
-                                <Route path='/history' component={ViewHistoryPage} />
-                                {/*<Route path='/:year' component={YearSelector}/> */}
-                                {/* <NavRedirect to='index' /> */}
-                                <Route path='/:year/index' component={withYearSelector(GameIndex)} />
-                                <Route path='/:year/makepicks' component={UserIsAuthenticated(withYearSelector(MakePicksPage))} />
-                                <Route path='/:year/picks' component={withYearSelector(ViewPicksPage)} />
-                                <Route path='/:year/scores' component={withYearSelector(ViewScoresPage)} />
-                                <Route path='/:year/admin/addwinner' component={withYearSelector(AdminViewPicksPage)}/>
-                                {/* Make sure this route is last */}
-                                <Route
-                                    path='*'
-                                    render={props => <PageNotFound {...props}
-                                        from='RoutingFallthrough'/>} />
-                            </Switch>
-                        </div>
-                    </Router>
+                    <PickemRouter/>
                 </DragDropContextProvider>
             </Provider>
         );
---
+++ pickem-react/src/Containers/PickemRouter.js
@@ -0,0 +1,68 @@
+import React, {
+    Component
+} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
+import ViewPicksPage from '../Containers/ViewPicksPage';
+import PickemApp from '../Containers/PickemApp';
+import GameIndex from '../Containers/GameIndex';
+import MakePicksPage from '../Containers/MakePicksPage';
+import ViewScoresPage from '../Containers/ViewScoresPage';
+import LoginPage from '../Containers/LoginPage';
+import LogoutPage from '../Containers/LogoutPage';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from '../auth.js';
+import PageNotFound from '../Components/PageNotFound';
+import AdminViewPicksPage from '../Containers/AdminViewPicksPage';
+import YearSelector from '../Containers/YearSelector';
+import ViewHistoryPage from '../Containers/ViewHistoryPage';
+
+function withYearSelector(WrappedComponent) {
+    class NewClass extends Component {
+        render() {
+            return (
+                <YearSelector {...this.props}>
+                    <WrappedComponent {...this.props} />
+                </YearSelector>
+            );
+        }
+    }
+    return NewClass;
+}
+
+class PickemRouter extends Component {
+    render() {
+        return (
+            <Router basename={process.env.REACT_APP_SITE_BASENAME}>
+                <div>
+                    <Route path='/' component={PickemApp} />
+                    <Switch>
+                        <Route exact path="/" render={() => (
+                            <Redirect to="/2018/index"/>
+                        )}/>
+                        <Route path='/login' component={UserIsNotAuthenticated(LoginPage)} />
+                        <Route path='/logout' component={UserIsAuthenticated(LogoutPage)} />
+                        <Route path='/history' component={ViewHistoryPage} />
+                        {/*<Route path='/:year' component={YearSelector}/> */}
+                        {/* <NavRedirect to='index' /> */}
+                        <Route path='/:year/index' component={withYearSelector(GameIndex)} />
+                        <Route path='/:year/makepicks' component={UserIsAuthenticated(withYearSelector(MakePicksPage))} />
+                        <Route path='/:year/picks' component={withYearSelector(ViewPicksPage)} />
+                        <Route path='/:year/scores' component={withYearSelector(ViewScoresPage)} />
+                        <Route path='/:year/admin/addwinner' component={withYearSelector(AdminViewPicksPage)}/>
+                        {/* Make sure this route is last */}
+                        <Route
+                            path='*'
+                            render={props => <PageNotFound {...props}
+                                from='RoutingFallthrough'/>} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default PickemRouter;
