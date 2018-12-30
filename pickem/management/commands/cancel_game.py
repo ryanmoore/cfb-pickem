512224ae0913b7e983f8e20198ce40bf7c9cce4d
Ryan Moore <ryanmoore88@gmail.com>
2018-12-30 10:21:48 -0500

    cancel_game command completed

--- pickem/management/commands/cancel_game.py
+++ pickem/management/commands/cancel_game.py
@@ -30,12 +30,45 @@ class Command(BaseCommand):
         with transaction.atomic():
             cancel_game_by_id(game_id, season_check=season)
 
+
 class ValidationError(Exception):
     pass
 
+
+def shift_wagers_over_deleted(wager):
+    """Changes all wagers that are in the same season with wager amounts
+    greater than the given wager to have a wager amount that is one less
+    :param Wager wager: the wager that will be deleted
+    """
+    wagers_to_adjust = Wager.objects.filter(
+        user=wager.user,
+        game__season=wager.game.season,
+        amount__gte=wager.amount)\
+        .exclude(game=wager.game).all()
+    for wager in wagers_to_adjust:
+        wager.amount -= 1
+        wager.save()
+
+
 def cancel_game_by_id(game_id, season_check=None):
+    """Cancels the given game for all users adjusting any wager greater
+    than the given one down and updating the fixed_wager_amount to be
+    the new max
+    :param int game_id: id of the game to cancel
+    :param int season_check: if provided, assert the game is in this year
+    """
     game_to_cancel = Game.objects.get(id=game_id)
     if season_check and game_to_cancel.season.year != season_check:
         raise ValidationError(
             'Game does not match given season: {} - {}'.format(
             game_to_cancel, season_check))
+    wagers_to_delete = Wager.objects.filter(game=game_to_cancel).all()
+    for wager in wagers_to_delete:
+        shift_wagers_over_deleted(wager)
+        wager.delete()
+    fixed_wager_games = Game.objects.filter(
+        fixed_wager_amount__gt=0).all()
+    for game in fixed_wager_games:
+        game.fixed_wager_amount -= 1
+        game.save()
+    game_to_cancel.delete()
--- pickem/management/commands/test_cancel_games.py
+++ pickem/management/commands/test_cancel_games.py
@@ -10,9 +10,9 @@ import pickem.management.commands.cancel_game as cmd_cancel_game
 
 class TestCancelGameCommand(TestCase):
     @staticmethod
-    def create_aware_datetime(day):
+    def create_aware_datetime(day, year=2018):
         return django_timezone.make_aware(
-            datetime.datetime(2018, 12, day),
+            datetime.datetime(year, 12, day),
             django_timezone.get_current_timezone())
 
     def setUp(self):
@@ -50,7 +50,7 @@ class TestCancelGameCommand(TestCase):
         self.game_championship = Game.objects.create(
             event=self.event_championship,
             datetime=self.create_aware_datetime(30),
-            fixed_wager_amount=4,
+            fixed_wager_amount=3,
             season=self.season,
         )
 
@@ -66,5 +66,151 @@ class TestCancelGameCommand(TestCase):
              '2000'),
             str(cm.exception))
 
+    def test_shift_wagers_over_deleted_shifts_down_greater(self):
+        wagers = [
+            Wager(user=self.user_high_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=3),
+            Wager(user=self.user_high_roller,
+                  game=self.game_puppy_bowl,
+                  amount=4),
+        ]
+        for wager in wagers:
+            wager.save()
+        cmd_cancel_game.shift_wagers_over_deleted(wagers[0])
+        puppy_wager = Wager.objects.get(id=wagers[1].id)
+        wager_to_delete = Wager.objects.get(id=wagers[0].id)
+        self.assertEqual(
+            3, puppy_wager.amount,
+            'The other wager with the greater amount should be shifted down')
+        self.assertEqual(
+            3, wager_to_delete.amount,
+            'The wager to delete\'s amount should be unchanged')
+
+    def test_shift_wagers_over_deleted_preserves_lower_amounts(self):
+        wagers = [
+            Wager(user=self.user_high_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=3),
+            Wager(user=self.user_high_roller,
+                  game=self.game_puppy_bowl,
+                  amount=2),
+        ]
+        for wager in wagers:
+            wager.save()
+        cmd_cancel_game.shift_wagers_over_deleted(wagers[0])
+        puppy_wager = Wager.objects.get(id=wagers[1].id)
+        wager_to_delete = Wager.objects.get(id=wagers[0].id)
+        self.assertEqual(
+            2, puppy_wager.amount,
+            'The other wager with the lesser amount should be unchanged')
+        self.assertEqual(
+            3, wager_to_delete.amount,
+            'The wager to delete\'s amount should be unchanged')
+
+    def test_shift_wagers_over_deleted_does_not_affect_other_users(self):
+        wagers = [
+            Wager(user=self.user_high_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=3),
+            Wager(user=self.user_low_roller,
+                  game=self.game_puppy_bowl,
+                  amount=4),
+        ]
+        for wager in wagers:
+            wager.save()
+        cmd_cancel_game.shift_wagers_over_deleted(wagers[0])
+        puppy_wager = Wager.objects.get(id=wagers[1].id)
+        wager_to_delete = Wager.objects.get(id=wagers[0].id)
+        self.assertEqual(
+            4, puppy_wager.amount,
+            'The wager by the other user should be unchanged')
+        self.assertEqual(
+            3, wager_to_delete.amount,
+            'The wager to delete\'s amount should be unchanged')
+
+    def test_shift_wagers_over_deleted_does_not_affect_other_seasons(self):
+        another_season = Season.objects.create(
+            year=1800, start_time=self.create_aware_datetime(25, year=1800))
+        another_season.save()
+        game_from_another_season = Game.objects.create(
+            event=self.event_first_responder_bowl,
+            datetime=self.create_aware_datetime(27, year=1800),
+            season=another_season)
+        game_from_another_season.save()
+
+        wagers = [
+            Wager(user=self.user_high_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=3),
+            Wager(user=self.user_high_roller,
+                  game=game_from_another_season,
+                  amount=4),
+        ]
+        for wager in wagers:
+            wager.save()
+        cmd_cancel_game.shift_wagers_over_deleted(wagers[0])
+        puppy_wager = Wager.objects.get(id=wagers[1].id)
+        wager_to_delete = Wager.objects.get(id=wagers[0].id)
+        self.assertEqual(
+            4, puppy_wager.amount,
+            'The wager by the same user in another season should be unchanged')
+        self.assertEqual(
+            3, wager_to_delete.amount,
+            'The wager to delete\'s amount should be unchanged')
+
     def test_cancel_game_by_id(self):
-        pass
+        wagers = [
+            Wager(user=self.user_high_roller,
+                  game=self.game_cereal_bowl,
+                  amount=1),
+            Wager(user=self.user_high_roller,
+                  game=self.game_puppy_bowl,
+                  amount=2),
+            Wager(user=self.user_high_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=3),
+            Wager(user=self.user_high_roller,
+                  game=self.game_championship,
+                  amount=3),
+            Wager(user=self.user_low_roller,
+                  game=self.game_cereal_bowl,
+                  amount=1),
+            Wager(user=self.user_low_roller,
+                  game=self.game_first_responder_bowl,
+                  amount=2),
+            Wager(user=self.user_low_roller,
+                  game=self.game_puppy_bowl,
+                  amount=3),
+            Wager(user=self.user_low_roller,
+                  game=self.game_championship,
+                  amount=3),
+        ]
+        for wager in wagers:
+            wager.save()
+        cmd_cancel_game.cancel_game_by_id(self.game_first_responder_bowl.id)
+        puppy_wager = Wager.objects.get(id=wagers[1].id)
+        expected_wagers = [
+            (self.user_high_roller, self.game_cereal_bowl, 1),
+            (self.user_high_roller, self.game_puppy_bowl, 2),
+            (self.user_high_roller, self.game_championship, 2),
+            (self.user_low_roller, self.game_cereal_bowl, 1),
+            (self.user_low_roller, self.game_puppy_bowl, 2),
+            (self.user_low_roller, self.game_championship, 2),
+        ]
+        for expected_wager in expected_wagers:
+            user, game, expected_amount = expected_wager
+            matching_wagers = Wager.objects.filter(
+                user=user, game=game).all()
+            self.assertEqual(1, len(matching_wagers),
+                             'Expect one wager per user per game')
+            actual, = matching_wagers
+            self.assertEqual(
+                expected_amount, actual.amount,
+                'Modified wager for {} does not match expected'.format(
+                    actual.game))
+
+        championship = Game.objects.get(id=self.game_championship.id)
+        self.assertEqual(
+            2, championship.fixed_wager_amount,
+            'Championship game\'s fixed wager not updated correctly')
